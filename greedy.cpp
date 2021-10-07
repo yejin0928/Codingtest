d6f0f17d6b088a6c8754574ef756c0b1589ff0f0
yejin <kwon990928@dongguk.edu>
2021-10-07 14:11:16 +0900

    add: greedy_2

--- greedy.cpp
+++ greedy.cpp
@@ -69,25 +69,193 @@
 // cout<<count-1+min2;
 // }
 //3보완
+// #include<iostream>
+// #include<algorithm>
+// #include<cmath>
+// using namespace std;
+// int main(){
+// int n,k;
+// int count=0;
+// cin>>n>>k;
+// while(n>=k){
+//   while(n%k!=0){
+//     n-=1;
+//     count++;
+//   }
+//   n/=k;
+//   count++;
+// }
+// while (n>1){
+//   n-=1;
+//   count++;
+// }
+// cout<<count<<endl;
+// }
+//기출1
+// #include <iostream>
+// #include <algorithm>
+// using namespace  std;
+// bool compare(int a, int b){
+//   return a>b;
+// }
+// int main(){
+//   int num, group=0, person=0;
+//   cin>>num;
+//   int array[num];
+//   for(int i=0; i<num; i++)
+//     cin>>array[i];
+//   sort(array, array+num);
+//   for(int i=0; i<num; i++)
+//   {
+//       person++;
+//       if(array[i]<=person){
+//           group++;
+//           person=0;}
+//   }
+//   cout<<group<<endl;
+// }
+// #include<iostream>
+// #include<string>
+// #include<vector>
+// using namespace std;
+// using std::stoi;
+
+// int main(){
+//   string s;
+//   int total;
+//   vector<int> v;
+//   getline(cin, s);
+
+//   string split="";
+//   for(int i=0; i<s.length(); i++){
+//     split=s.at(i);
+//     v.push_back(atoi(split.c_str()));
+//   }
+//   for(int i=0; i<v.size(); i++)
+//     cout<<v[i]<<" ";
+//   if(v[0]==0)
+//     total = 0;
+//   else
+//     total=v[0];
+//   for (int i=0; i<v.size(); i++){
+//   if(v[i]==0&&(i+1)!=v.size())
+//         total +=v[i+1];
+//   else{
+//     total *=v[i+1];
+//   }
+// }
+//     cout<<total<<endl;
+// }
+//기출3
+// #include<iostream>
+// #include<cstring>
+// using namespace std;
+// int main(){
+//   string s;
+//   char c_s[1000000];
+//   int count[100000];
+//   int count_v[10000];
+//   int count_i=0;
+//   int value=1;
+//   int flag;
+//   int flag_v=1;
+//   getline(cin, s);
+//   strcpy(c_s, s.c_str());
+  
+//   for(int i=1; i<s.length(); i++){
+//     if((int)c_s[i-1]==(int)c_s[i]){
+//         value++;
+//         count[count_i]=value;
+//         count_v[count_i]=(int)c_s[i]-48;
+//   }else{
+//         value=1;
+//         count_i++;
+//   }
+//   }
+// flag=count_v[0];
+//  for(int i=1; i<=count_i; i++){
+//    if(flag==count_v[i])
+//     flag_v++;
+//  }
+//  if(flag_v>=count_i-flag_v)
+//     cout<<count_i-flag_v+1<<endl;
+//   else
+//     cout<<flag_v+1<<endl;
+// }
+//기출4
+// #include<iostream>
+// #include<algorithm>
+// #include<vector>
+// using namespace std;
+// bool value(int a, int b){return a>b;}
+// int main(){
+//   int count;
+//   int flag=0;
+//   cin>>count;
+//   int array[count];
+//   for(int i=0; i<count;i++){
+//     cin>>array[i];
+//   }
+//   sort(array,array+count);
+//   int total=0;
+// for(int i=0;i<count; i++){
+//     total+=array[i];
+//   }
+//   int fin=1;
+//   int current=0;
+//   vector<int> v;
+
+//   while(fin<=count){
+//     for(int i=0; i<count; i++){
+//       v.push_back(array[i]);
+//     }
+//   }
+// }
+//기출5
+// #include<iostream>
+// using namespace std;
+// int main(){
+//   int count, weight;
+//   int total=0;
+//   cin>>count>>weight;
+//   int array[count];
+//   for(int i=0; i<count; i++)
+//     cin>>array[i];
+//   for(int i=0; i<count; i++)
+//       for(int j=i+1; j<count;j++)
+//         if(array[i]!=array[j])
+//             total++;
+//   cout<<total<<endl;
+// }
+//기출6
+
+#include <string>
+#include <vector>
 #include<iostream>
-#include<algorithm>
-#include<cmath>
 using namespace std;
+
+int solution(vector<int> food_times, long long k) {
+    int answer = 0;
+    int j=0;
+   while(j<=k){
+       if(food_times[j%3]!=0){
+           food_times[j%3]-=1;
+           j++;
+           if(food_times[j%3]==0)
+               j++;
+       }else{
+           j +=2;
+       }
+   }
+    for(int i=0; i<food_times.size(); i++)
+        if(food_times[i]!=0){
+            answer=i+1;
+            break;
+        }
+    return answer;
+}
 int main(){
-int n,k;
-int count=0;
-cin>>n>>k;
-while(n>=k){
-  while(n%k!=0){
-    n-=1;
-    count++;
-  }
-  n/=k;
-  count++;
-}
-while (n>1){
-  n-=1;
-  count++;
-}
-cout<<count<<endl;
+  vector<int> v={1,3,2};
+  int k=5;
+  cout<<solution(v,k);
 }
